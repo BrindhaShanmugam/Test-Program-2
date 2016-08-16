9ef5234629557e4966880c43786b586f6ca92829
BrindhaShanmugam <brindhakartheepan@gmail.com>
2016-08-16 18:57:12 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Alphabet{
+public static void main(String ag[]){
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+char c[]=s1.toCharArray();
+if(c[0]>='a'&&c[0]<='z'||c[0]>='A'&&c[0]<='Z')
+{
+System.out.println("The given character is alphabet");
+}
+else
+System.out.println("The given character is not an alphabet");
+}
+}
