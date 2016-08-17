d49bf0407c5f65e287f759db9df1804f8b9d39b3
BrindhaShanmugam <brindhakartheepan@gmail.com>
2016-08-17 19:00:06 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Hello{
+public static void main(String ag[]){
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+int n=s1.length();
+for(int i=0;i<n;i++)
+System.out.println(s1);
+}}
